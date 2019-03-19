ce3565f769b32dc70364425f55dcda4d6f1d1ed7
lucas <lucas@Lucass-MacBook-Pro.local>
2019-03-20 00:04:36 +0800

    feat(shutil):add shutil test

---
+++ app/script/shutil_base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : shutil_base.py
+# @Author: lucas
+# @Date  : 3/19/19
+# @Desc  :
+
+import shutil
+import os
+
+
+def copy(source_path=None, file_path=None, new_file_path=None):
+    if not source_path:
+        return
+    os.chdir(source_path)
+    shutil.copy(file_path, new_file_path)
+
+
+def run():
+    copy(source_path=u'/Users/lucas/projects/elizabeth', file_path=u'README.md', new_file_path='README-2.md')
+
+
+if __name__ == '__main__':
+    run()
